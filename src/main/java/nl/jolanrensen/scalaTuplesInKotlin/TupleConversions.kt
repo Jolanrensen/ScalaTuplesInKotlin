f64ec6e3d6a5f607d94eed4b4b6c6c7be99c2209
Jolanrensen <j.j.r.rensen@student.tue.nl>
2021-02-28 21:33:04 +0100

    started with some kdoc

--- src/main/java/nl/jolanrensen/scalaTuplesInKotlin/TupleConversions.kt
+++ src/main/java/nl/jolanrensen/scalaTuplesInKotlin/TupleConversions.kt
@@ -9,8 +9,23 @@ import scala.Tuple3
  * by Jolan Rensen, 18-02-2021
  */
 
+/**
+ * Returns a new [Tuple3] based on the arguments in the current [Triple].
+ */
 fun <T1, T2, T3> Triple<T1, T2, T3>.toTuple(): Tuple3<T1, T2, T3> = Tuple3<T1, T2, T3>(first, second, third)
+
+/**
+ * Returns a new [Triple] based on the arguments in the current [Tuple3].
+ */
 fun <T1, T2, T3> Tuple3<T1, T2, T3>.toTriple(): Triple<T1, T2, T3> = Triple<T1, T2, T3>(_1(), _2(), _3())
 
+
+/**
+ * Returns a new [Tuple2] based on the arguments in the current [Pair].
+ */
 fun <T1, T2> Pair<T1, T2>.toTuple(): Tuple2<T1, T2> = Tuple2<T1, T2>(first, second)
+
+/**
+ * Returns a new [Pair] based on the arguments in the current [Tuple2].
+ */
 fun <T1, T2> Tuple2<T1, T2>.toPair(): Pair<T1, T2> = Pair<T1, T2>(_1(), _2())
--- src/main/java/nl/jolanrensen/scalaTuplesInKotlin/building/BuildTupleBuilders.kt
+++ src/main/java/nl/jolanrensen/scalaTuplesInKotlin/building/BuildTupleBuilders.kt
@@ -29,21 +29,34 @@ private fun main() {
 
     for (a in alphabet) {
         val numbers = (1..a).toList()
-        for (nr in numbers) {
-            println(
-                "fun <${numbers.joinToString { "T$it" }}> tupleOf(${numbers.joinToString { "${ words[it - 1] }: T$it" }}): Tuple$a<${numbers.joinToString { "T$it" }}> = Tuple$a<${numbers.joinToString { "T$it" }}>(${numbers.joinToString { words[it - 1] }})"
-            )
-        }
+        println("""
+            
+                /** 
+                 * Returns a new Tuple$a of the given arguments. 
+                 * @see [t] 
+                 **/""".trimIndent()
+        )
+        println(
+            "fun <${numbers.joinToString { "T$it" }}> tupleOf(${numbers.joinToString { "${ words[it - 1] }: T$it" }}): Tuple$a<${numbers.joinToString { "T$it" }}> = Tuple$a<${numbers.joinToString { "T$it" }}>(${numbers.joinToString { words[it - 1] }})"
+        )
+
     }
 
     println()
 
     for (a in alphabet) {
         val numbers = (1..a).toList()
-        for (nr in numbers) {
-            println(
-                "fun <${numbers.joinToString { "T$it" }}> t(${numbers.joinToString { "${ words[it - 1] }: T$it" }}): Tuple$a<${numbers.joinToString { "T$it" }}> = Tuple$a<${numbers.joinToString { "T$it" }}>(${numbers.joinToString { words[it - 1] }})"
-            )
-        }
+
+        println("""
+            
+                /** 
+                 * Returns a new Tuple$a of the given arguments. 
+                 * @see [tupleOf] 
+                 **/""".trimIndent()
+        )
+        println(
+            "fun <${numbers.joinToString { "T$it" }}> t(${numbers.joinToString { "${ words[it - 1] }: T$it" }}): Tuple$a<${numbers.joinToString { "T$it" }}> = Tuple$a<${numbers.joinToString { "T$it" }}>(${numbers.joinToString { words[it - 1] }})"
+        )
+
     }
 }
