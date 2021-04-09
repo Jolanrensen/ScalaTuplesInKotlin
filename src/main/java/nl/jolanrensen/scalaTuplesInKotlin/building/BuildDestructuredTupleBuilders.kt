5e0de4fb49dbfca5ca790000600559619da773d4
Jolanrensen <j.j.r.rensen@student.tue.nl>
2021-04-09 15:37:29 +0200

    added toList and more tests

--- src/main/java/nl/jolanrensen/scalaTuplesInKotlin/building/BuildDestructuredTupleBuilders.kt
+++ src/main/java/nl/jolanrensen/scalaTuplesInKotlin/building/BuildDestructuredTupleBuilders.kt
@@ -24,21 +24,29 @@ private fun main() {
 
     println()
 
-    // { tuple } u value
-    println("infix fun <T1 : Product, T2> (() -> T1).u(next: T2): Tuple2<T1, T2> = Tuple2<T1, T2>(this(), next)")
+    // value u tuple: Same as tuple u value
+    // { value }: Not done, use { value.u }
+    // tuple u tuple: Not done, use tuple u value u value... or {}
 
     // value u { tuple }
     println("infix fun <T1, T2 : Product> T1.u(next: () -> T2): Tuple2<T1, T2> = Tuple2<T1, T2>(this, next())")
 
-    // { tuple } u { tuple }
-    println("infix fun <T1 : Product, T2: Product> (() -> T1).u(next: () -> T2): Tuple2<T1, T2> = Tuple2<T1, T2>(this(), next())")
+    // { tuple } u value
+    println("infix fun <T1 : Product, T2> (() -> T1).u(next: T2): Tuple2<T1, T2> = Tuple2<T1, T2>(this(), next)")
 
-    // tuple u { value }
+    // tuple u { tuple }
     for (a in alphabet) {
         val numbers = (1..a).toList()
 
         println("infix fun <${numbers.joinToString { "T$it" }}: Product> Tuple${a - 1}<${numbers.dropLast(1).joinToString { "T$it" }}>.u(next: () -> T$a): Tuple$a<${numbers.joinToString { "T$it" }}> = Tuple$a<${numbers.joinToString { "T$it" }}>(${numbers.dropLast(1).joinToString { "this._$it()" }}, next())")
     }
+
+    // { tuple } u tuple: Not done, use { tuple } u value u value
+
+    // { tuple } u { tuple }
+    println("infix fun <T1 : Product, T2: Product> (() -> T1).u(next: () -> T2): Tuple2<T1, T2> = Tuple2<T1, T2>(this(), next())")
+
+
 
 
 
